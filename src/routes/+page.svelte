ac517a6bbbe26a93fb1af14125a4d715b2867c1b
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-08-29 13:13:51 -0400

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import { onMount } from 'svelte'
   import { refresh } from '../modules/refresh'
   import { indexStore } from '../modules/index'
-  import { validatorList } from '../modules/query_view'
+  import { validatorList, vdfDifficulty } from '../modules/query_view'
   import { makeUrl } from '../modules/api'
   import {apiError} from '../modules/error'
 
@@ -32,7 +32,7 @@
     {$apiError}
   {/if}
 
-  <div class="uk-flex">
+  <div class="uk-flex uk-flex-wrap">
     {#if $indexStore}
       <Card title="metadata">
         <div slot="body">
@@ -47,11 +47,21 @@
       <Card title="validators">
         <div slot="body">
           {#each $validatorList as v}
-            <div>{v}</div>
+            <div>{v.slice(0,7)}</div>
           {/each}
         </div>
       </Card>
     {/if}
+
+    {#if $vdfDifficulty}
+      <Card title="vdf">
+        <div slot="body">
+          <p> difficulty: {$vdfDifficulty[0]} </p>
+          <p> security: {$vdfDifficulty[1]} </p>
+        </div>
+      </Card>
+    {/if}
+
   </div>
 
   <!-- <div>
--- src/ui/Card.svelte
+++ src/ui/Card.svelte
@@ -2,7 +2,7 @@
   export let title
 </script>
 
-<div class="uk-card uk-card-default uk-card-body uk-margin-left">
+<div class="uk-card uk-card-default uk-card-body uk-margin-left uk-margin-bottom">
   <h5 class="uk-card-title">{title}</h5>
   <slot name="body"/>
 </div>
