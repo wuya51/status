8f9b68defa45ebe24c122d1dd0f7b10298b630ac
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-10-27 11:43:21 -0400

    Updates

--- src/ui/Card.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export let title
-  export let style = "default"
-</script>
-
-<div class="uk-card uk-card-{style} uk-card-body uk-margin-left uk-margin-bottom">
-  <h5 class="uk-card-title">{title}</h5>
-  <slot name="body"/>
-</div>
--- src/utils/coin.ts
+++
@@ -1,3 +0,0 @@
-export const scaleCoin = (unscaled: number): number => {
-  return unscaled / 1_000_000
-}
