8b8d7f01fdd783e4a677aabf5f9d89d4d9c3c11f
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-11-15 10:28:13 -0500

    patch balance API

--- src/api/payloads/common.ts
+++ src/api/payloads/common.ts
@@ -1,7 +1,7 @@
 import type { ViewObj } from './types'
 
 export const account_balance_payload = (address: string): ViewObj => ({
-  function: '0x1::slow_wallet::balance',
+  function: '0x1::ol_account::balance',
   type_arguments: [],
   arguments: [address],
 })
